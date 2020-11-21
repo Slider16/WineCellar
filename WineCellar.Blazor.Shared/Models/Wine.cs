9fa06d49639bab3821f42fc6f1bef72e1b9cb684
Elgin Rogers <elginr16@gmail.com>
2020-11-21 11:02:27 -0600

    Adds WinCellar.Blazor.Shared project and a beginning Wine model class.

---
+++ WineCellar.Blazor.Shared/Models/Wine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WineCellar.Blazor.Shared.Models
+{
+    public class Wine
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Vineyard { get; set; }
+        public string Location { get; set; }
+        public int Year { get; set; }
+        public string Notes { get; set; }
+    }
+}
